549d2ede7e1b8d4c8b308e9ea4535bb9594abe41
Eduardo Braga <chocolate.blindado123@gmail.com>
2022-12-08 18:15:10 -0300

    Feat: Criei função para mostrar a thumbnail

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const form = document.querySelector('#form')
+const containerThumbnails = document.querySelector('.container-thumbnails')
+const imgThumbnail = document.querySelector('.img-thumbnail')
 
 const getThumbnailsVideo = async idVideo => {
   const KEYapi = "AIzaSyBNtmsQjdd06IEHyNCTv0A5XRmez7_tIHw"
@@ -28,6 +30,17 @@ const getIdVideo = link => {
   return link.slice(firstCut, lastIndex)
 }
 
+const showContainerThumbnails = thumbnails => {
+  const urlThumbnailDefault = thumbnails.default.url //120 x 90
+  const urlThumbnailMedium = thumbnails.medium.url //320 x 180
+  const urlThumbnailHigh = thumbnails.high.url //480 x 360
+  const urlThumbnailStandard = thumbnails.standard.url //640 x 480
+  const urlThumbnailMaxres = thumbnails.maxres.url //1280 x 720
+
+  imgThumbnail.src = urlThumbnailMaxres
+  console.log(thumbnails)
+}
+
 form.addEventListener('submit', async event => {
   event.preventDefault()
 
@@ -36,5 +49,5 @@ form.addEventListener('submit', async event => {
 
   const thumbnails = await getThumbnailsVideo(idVideo)
 
-  console.log(thumbnails)
+  showContainerThumbnails(thumbnails)
 })

f5cfb58825c2a7b729b7118d2590b04c1401bb41
Eduardo Braga <chocolate.blindado123@gmail.com>
2022-12-09 11:13:44 -0300

    Feat: Criei função para pegar as URLS

--- index.js
+++ index.js
@@ -4,20 +4,19 @@ const imgThumbnail = document.querySelector('.img-thumbnail')
 
 const getThumbnailsVideo = async idVideo => {
   const KEYapi = "AIzaSyBNtmsQjdd06IEHyNCTv0A5XRmez7_tIHw"
-  const URL =
-    `https://www.googleapis.com/youtube/v3/videos?id=${idVideo}&key=${KEYapi}&part=snippet`
+  const URL = `https://www.googleapis.com/youtube/v3/videos?id=${idVideo}&key=${KEYapi}&part=snippet`
 
-  try{
+  try {
     const response = await fetch(URL)
 
-    if(!response.ok){
+    if (!response.ok) {
       throw new Error('video não encontrado')
     }
 
     const video = await response.json()
 
     return video.items[0].snippet.thumbnails
-  }catch(error){
+  } catch (error) {
     console.log(error.message)
   }
 }
@@ -30,15 +29,28 @@ const getIdVideo = link => {
   return link.slice(firstCut, lastIndex)
 }
 
-const showContainerThumbnails = thumbnails => {
-  const urlThumbnailDefault = thumbnails.default.url //120 x 90
-  const urlThumbnailMedium = thumbnails.medium.url //320 x 180
-  const urlThumbnailHigh = thumbnails.high.url //480 x 360
-  const urlThumbnailStandard = thumbnails.standard.url //640 x 480
-  const urlThumbnailMaxres = thumbnails.maxres.url //1280 x 720
+const getUrlThumbnails = thumbnails => {
+  const sizesThumbails = ['default', 'medium', 'high', 'standard', 'maxres']
+  const URLs = []
 
+  sizesThumbails.forEach(type => {
+    const sizeExists = thumbnails[type] !== undefined
+
+    if(sizeExists){
+      URLs.push({[type]: thumbnails[type].url})
+    }
+  })
+
+  return URLs
+}
+
+const showContainerThumbnails = thumbnails => {
+  containerThumbnails.style = 'display: grid'
+
+  const URLS = getUrlThumbnails(thumbnails)
+
+  const urlThumbnailMaxres = thumbnails.maxres.url //1280 x 720
   imgThumbnail.src = urlThumbnailMaxres
-  console.log(thumbnails)
 }
 
 form.addEventListener('submit', async event => {
@@ -48,6 +60,11 @@ form.addEventListener('submit', async event => {
   const idVideo = getIdVideo(link)
 
   const thumbnails = await getThumbnailsVideo(idVideo)
+
+  if(!thumbnails){
+    console.log('thumbnails não existente')
+    return
+  }
 
   showContainerThumbnails(thumbnails)
 })
